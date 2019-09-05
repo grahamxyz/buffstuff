42a46526ff301b7ff683f00e45e3df7322c02449
GrahamHill@BSU <GrahamHill@u.boisestate.edu>
2019-09-05 12:02:01 -0600

    buff's stuff @  2019-09-05	12:02:01	MDT

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,13 +1,13 @@
     
 #!/bin/sh
 
-#copy buff's slides.pdf
+#copy buff's slides.pdf from my local dir
 
 cp -u ../buff-public/slides/slides.pdf ../
 
-
-
 #Push
 git add ../*
+
+# Timestamp this baby and push the commit
 git commit -m "buff's stuff @  $(date +%Y-%m-%d%t%T%t%Z)"	
-git push
+git push -u origin master
