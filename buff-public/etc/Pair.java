5333cac7c8544fa5239b65b76ed84d1a69620de3
GrahamHill@BSU <GrahamHill@u.boisestate.edu>
2019-09-14 06:00:03 -0600

    buff's stuff @  2019-09-14	06:00:03	MDT

--- buff-public/etc/Pair.java
+++ buff-public/etc/Pair.java
@@ -4,13 +4,17 @@ public class Pair {
 
     private Object car, cdr;
 
-    public Pair(Object car, Object cdr) { this.car=car; this.cdr=cdr; }
+    private Pair(Object car, Object cdr)
+        { this.car=car; this.cdr=cdr; }
+
+    public static Pair cons(Object car, Object cdr)
+        { return new Pair(car,cdr); }
 
     public Object car() { return this.car; }
     public Object cdr() { return this.cdr; }
 
     public static void main(String[] args) {
-	Pair p=new Pair(2,3);
+	Pair p=cons(2,3);
 	System.out.println(p.car());
 	System.out.println(p.cdr());
     }
